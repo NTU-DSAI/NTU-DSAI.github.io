5e2363e3d2b2241be89d0380b5b461e17232a871
majulahsingapuri <majulahsingapuri@users.noreply.github.com>
2022-04-02 10:24:40 +0000

    deploy: 5d09ef6b98bcfaa07d97991e2f596ebb51b0b6b6

---
+++ assets/js/30a24c52.48990694.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkntu_dsai_github_io=self.webpackChunkntu_dsai_github_io||[]).push([[453],{8605:function(l){l.exports=JSON.parse('{"allTagsPath":"/blog/tags","slug":"/blog/tags/hello","name":"hello","count":1,"permalink":"/blog/tags/hello"}')}}]);
---
+++ assets/js/814f3328.1d267ac1.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkntu_dsai_github_io=self.webpackChunkntu_dsai_github_io||[]).push([[2535],{5641:function(e){e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Welcome","permalink":"/blog/welcome"}]}')}}]);
