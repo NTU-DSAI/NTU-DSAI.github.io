85dd87dfd2f5b0b7633b51905ace3638d7ea4cfc
Bhargav Singapuri <bhsingapuri@gmail.com>
2022-05-04 12:53:03 +0800

    Add Google Tracking and Sitemap for Analytics

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -32,6 +32,10 @@ const config = {
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
         },
+        gtag: {
+          trackingID: "G-CE1T51312P",
+          anonymizeIP: true,
+        },
       }),
     ],
   ],
